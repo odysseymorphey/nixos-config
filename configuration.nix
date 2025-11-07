dfb61d2a87dfac0db35f0fa891e69f7e2a47eb09
fredacha <ramazanov-zalim@mail.ru>
2025-11-08 00:26:52 +0300

    .

--- configuration.nix
+++ configuration.nix
@@ -104,7 +104,7 @@
 
   environment.systemPackages = with pkgs; [
     git neovim wget curl
-    gnome.gnome-tweaks gnomeExtensions.dash-to-dock
+    pkgs.gnome-tweaks gnomeExtensions.dash-to-dock
     gnomeExtensions.appindicator gnomeExtensions.blur-my-shell
     gnomeExtensions.user-themes
     xdg-utils
