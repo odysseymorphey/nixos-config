2667ae4147f8c00b76f814e5176d0079ed2c255a
fredacha <ramazanov-zalim@mail.ru>
2025-11-08 16:44:17 +0300

    .

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 {
   description = "Nix configs + dev shell with nil & alejandra";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs = {
     self,
     nixpkgs,
   }: let
     forAll = f:
-      nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"] (system:
-        f (import nixpkgs {inherit system;}));
+      nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"] (
+        system: f (import nixpkgs {inherit system;})
+      );
   in {
+    formatter = forAll (pkgs: pkgs.alejandra);
     devShells = forAll (pkgs: {
       default = pkgs.mkShell {
         packages = with pkgs; [
--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -3,8 +3,7 @@
   pkgs,
   lib,
   ...
-}:
-{
+}: {
   imports = [
     ./hardware-configuration.nix
   ];
